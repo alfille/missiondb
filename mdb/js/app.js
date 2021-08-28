530ae8a060bd33a994715953aec17fc0d89a5376
alfille <paul.alfille@gmail.com>
2021-08-28 14:13:50 -0400

    Update app.js

--- mdb/js/app.js
+++ mdb/js/app.js
@@ -219,7 +219,6 @@ var displayTable = new dataTable( "PatientTable", null, ["_id", "title", "text",
   function showPatientList() {
     db.allDocs({include_docs: true, descending: true}).then( function(doc) {
 	  displayTable.fill(doc.rows) ;
-      redrawPatientList(doc.rows);
     }).catch( function(err) {
       console.log(err);
     });
@@ -292,62 +291,12 @@ var displayTable = new dataTable( "PatientTable", null, ["_id", "title", "text",
     }
   }
 
-  // Given an object representing a todo, this will create a list item
-  // to display it.
-  function createPatientListRow(todo) {
-    var checkbox = document.createElement('input');
-    checkbox.className = 'toggle';
-    checkbox.type = 'checkbox';
-    checkbox.addEventListener('change', checkboxChanged.bind(this, todo));
-
-    var label = document.createElement('label');
-    label.appendChild( document.createTextNode(getProp(todo,"title")));
-    label.addEventListener('dblclick', todoDblClicked.bind(this, todo));
-
-    var deleteLink = document.createElement('button');
-    deleteLink.className = 'destroy';
-    deleteLink.addEventListener( 'click', deleteButtonPressed.bind(this, todo));
-
-    var divDisplay = document.createElement('div');
-    divDisplay.className = 'view';
-    divDisplay.appendChild(checkbox);
-    divDisplay.appendChild(label);
-    divDisplay.appendChild(deleteLink);
-
-    var inputEditTodo = document.createElement('input');
-    inputEditTodo.id = 'input_' + todo._id;
-    inputEditTodo.className = 'edit';
-    inputEditTodo.value = todo.title;
-    inputEditTodo.addEventListener('keypress', todoKeyPressed.bind(this, todo));
-    inputEditTodo.addEventListener('blur', todoBlurred.bind(this, todo));
-
-    var li = document.createElement('li');
-    li.id = 'li_' + todo._id;
-    li.appendChild(divDisplay);
-    li.appendChild(inputEditTodo);
-
-    if (todo.completed) {
-      li.className += 'complete';
-      checkbox.checked = true;
-    }
-
-    return li;
-  }
-  
   function getProp( obj, prop ) {
     if (prop in obj ) {
       return obj[prop] ;
     } else {
       return prop+"?" ;
     }
-  }
-
-  function redrawPatientList(todos) {
-    var ul = document.getElementById('patient-list');
-    ul.innerHTML = '';
-    todos.forEach(function(todo) {
-      ul.appendChild(createPatientListRow(todo.doc));
-    });
   }
 
   function newTodoKeyPressHandler( event ) {
