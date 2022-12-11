ae4791755dc65f6a4d8e35dd6b096153b26d15e7
alfille <paul.alfille@gmail.com>
2022-12-11 09:36:48 -0500

    Rem ove some demo code

--- minimal-react-master/js/main.js
+++
@@ -1,5 +0,0 @@
-/* A one-liner, you can use inline js in html if you like */
-ReactDOM.render(
-  React.createElement(HelloWorldBanner), 
-  document.getElementById('content')
-)
--- minimal-react-master/other_examples/with_jsx/js/main.js
+++
@@ -1,5 +0,0 @@
-/* A one-liner, you can use inline js in html if you like */
-ReactDOM.render(
-  <HelloWorldBanner/>,
-  document.getElementById('content')
-)
